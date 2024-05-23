e54ad33b7e695fd91f7e2212edb1bdd13ea391c6
Letícia Neves <iamleticianeves@gmail.com>
2024-05-23 19:40:53 -0300

    Create vencedor.js

---
+++ vencedor.js
@@ -0,0 +1,19 @@
+const { checkForWinner } = require('./game');
+
+test('Detecção de vitória do jogador 1', () => {
+    let gameState = {
+        score: { player1: 10, player2: 5 },
+        winningScore: 10
+    };
+    const winner = checkForWinner(gameState);
+    expect(winner).toBe('player1');
+});
+
+test('Detecção de vitória do jogador 2', () => {
+    let gameState = {
+        score: { player1: 8, player2: 10 },
+        winningScore: 10
+    };
+    const winner = checkForWinner(gameState);
+    expect(winner).toBe('player2');
+});
