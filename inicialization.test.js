7f1067137771eb6a5b6a713cc53a7bba9e6cde86
Letícia Neves <iamleticianeves@gmail.com>
2024-05-26 14:41:13 -0300

    Delete inicialization.test.js

--- inicialization.test.js
+++
@@ -1,9 +0,0 @@
-const { initGame } = require('./game');
-
-test('Inicialização do jogo', () => {
-    const gameState = initGame();
-    expect(gameState.player1Position).toBe(50); 
-    expect(gameState.player2Position).toBe(50); 
-    expect(gameState.ballPosition).toEqual({ x: 50, y: 50 }); 
-    expect(gameState.score).toEqual({ player1: 0, player2: 0 });
-});
