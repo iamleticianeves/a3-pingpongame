0235586087a8411f952a9e26895e69e782f09085
Letícia Neves <iamleticianeves@gmail.com>
2024-05-26 14:41:43 -0300

    Delete colision.js

--- colision.js
+++
@@ -1,8 +0,0 @@
-const { updateBallPosition } = require('./game'); 
-
-test('Colisão da bola com a borda superior', () => {
-    let gameState = { ballPosition: { x: 50, y: 1 }, ballDirection: { x: 1, y: -1 } };
-    gameState = updateBallPosition(gameState);
-    expect(gameState.ballDirection.y).toBe(1);
-});
-
